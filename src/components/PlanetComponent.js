7f2afd50669886f3630db2fb1fb9e8725d130aa3
Kiran755 <kirandevadiga441@gmail.com>
2022-08-19 14:18:08 +0530

    modified planet Components a little bit

--- src/components/PlanetComponent.js
+++ src/components/PlanetComponent.js
@@ -1,14 +1,38 @@
-import React from 'react'
+import { React, useEffect, useState } from 'react'
 import earth from "./assets/earth.svg";
 import "./PlanetComponent.css";
 import Sun from "./assets/Sun.svg";
-import mercury from "./assets/mercury.svg"
+import Mercury from './Mercury';
+import Earth from './Earth';
+import { motion } from "framer-motion";
 
 const PlanetComponent = () => {
+    const [planetNo, setPlanetNo] = useState(0);
+    const Increment = () => {
+        setPlanetNo(planetNo + 1);
+    }
+    const Decrement = () => {
+        setPlanetNo(planetNo - 1);
+    }
+    const HandlePlanet = () => {
+        switch (planetNo) {
+            case 1:
+                return (
+                    <Mercury />
+                )
+
+            case 2:
+                return <Earth />
+            default:
+                return <Earth />
+        }
+    }
     return (
         <div className='PlanetHolder'>
             <div className='planet'>
-                <img src={mercury} className="planetSource" alt="earth.svg"></img>
+                <i className="fa-solid fa-angles-left" onClick={Increment}></i>
+                {HandlePlanet()}
+                <i className="fa-solid fa-angles-right" onClick={Decrement}></i>
                 <h4>Earth</h4>
             </div>
             <div className='Sun'>
