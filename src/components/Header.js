e4655668f704c9ce8ded8c6eca2ee172c1578ab9
Kiran755 <kirandevadiga441@gmail.com>
2022-08-19 14:14:20 +0530

    Modified the navbar to be hidable

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,46 +1,80 @@
 import React, { useState } from 'react'
 import { Navbar, NavbarToggler, Collapse, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap'
+import "./Header.css";
+import { motion, AnimatePresence } from "framer-motion";
 const Header = () => {
-    const [open, setOpen] = useState("false");
+    const [open, setOpen] = useState(false);
+    const [hide, setHide] = useState(false);
     const ToggleMenu = () => {
         setOpen(!open);
     }
+    const Togglenavbar = () => {
+        setHide(!hide);
+        console.log(hide);
+    }
     return (
         <>
-            <Navbar color="dark" dark expand="md">
-                <div className='container'>
-                    <div className='row'>
-                        <NavbarBrand className="mr-auto">
-                            <img src='Asteroid.png' alt="asteroid" height="60"
-                                width="60"></img>
-                        </NavbarBrand>
-                        <NavbarToggler onClick={ToggleMenu} />
-                        <Collapse isOpen={open} navbar>
-                            <Nav navbar>
-                                <NavItem>
-                                    <NavLink className="nav-link" to="/home">Home</NavLink>
-                                </NavItem>
-                                <NavItem>
-                                    <NavLink className="nav-link" to="/home">Home</NavLink>
+            <AnimatePresence>
+                {!hide ?
+                    (
+                        <motion.div className="HeaderNav"
+                            initial={{ y: "-10vh", opacity: 0 }}
+                            animate={{ y: 0, opacity: 1 }}
+                            exit={{ y: "-10vh", opacity: 0 }}
+                            transition={{ duration: 0.8, type: "spring", stiffness: 120 }}
+                        >
+                            <Navbar color="dark" dark expand="md">
+                                <div className='container'>
+                                    <div className='row'>
+                                        <NavbarBrand className="mr-auto">
+                                            <img src='Asteroid.png' alt="asteroid" height="60"
+                                                width="60"></img>
+                                        </NavbarBrand>
+                                        <NavbarToggler onClick={ToggleMenu} />
+                                        <Collapse isOpen={open} navbar>
+                                            <Nav navbar>
+                                                <NavItem>
+                                                    <NavLink className="nav-link" to="/home">Home</NavLink>
+                                                </NavItem>
+                                                <NavItem>
+                                                    <NavLink className="nav-link" to="/home">Home</NavLink>
 
-                                </NavItem>
-                                <NavItem>
-                                    <NavLink className="nav-link" to="/home">Home</NavLink>
-                                </NavItem>
-                                <NavItem>
+                                                </NavItem>
+                                                <NavItem>
+                                                    <NavLink className="nav-link" to="/home">Home</NavLink>
+                                                </NavItem>
+                                                <NavItem>
 
-                                    <NavLink className="nav-link" to="/home">Home</NavLink>
+                                                    <NavLink className="nav-link" to="/home">Home</NavLink>
 
-                                </NavItem>
-                                <NavItem>
-                                    <NavLink className="nav-link" to="/home">Home</NavLink>
-                                </NavItem>
-                            </Nav>
-                        </Collapse>
+                                                </NavItem>
+                                                <NavItem>
+                                                    <NavLink className="nav-link" to="/home">Home</NavLink>
+                                                </NavItem>
+                                            </Nav>
+                                        </Collapse>
 
-                    </div>
-                </div>
-            </Navbar>
+                                    </div>
+                                </div>
+                            </Navbar>
+
+                        </motion.div>
+                    ) : (null)}
+            </AnimatePresence>
+
+
+            <div className='closeNav'>
+                <button onClick={Togglenavbar} className="ToggleButtonNavbar">
+                    {!hide ? (<motion.div>
+                        <i className="fa-solid fa-angles-up AngleUp"></i>
+                    </motion.div>) : (<motion.div>
+                        <i className="fa-solid fa-angles-down AngleDown"></i>
+                    </motion.div>)}
+
+
+
+                </button>
+            </div>
         </>
     )
 }
