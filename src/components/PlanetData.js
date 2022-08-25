e2a947137a5a5bc232d409267283322041a3384b
Kiran755 <kirandevadiga441@gmail.com>
2022-08-26 02:35:49 +0530

    PlanetData.js

---
+++ src/components/PlanetData.js
@@ -0,0 +1,10 @@
+const Data = [
+    {
+        id: 1,
+        PlanetName: "Earth",
+        NaturalSatellite: "1",
+        NaturalSatelliteName: "Moon",
+        PlanetDesc: "lorem ipsum dolor aw dawdawa"
+    }
+];
+export default Data;
