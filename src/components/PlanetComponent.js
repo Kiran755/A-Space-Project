e768a4efb92c3f90fefeb7dca0b6b55eed4d61e1
Kiran755 <kirandevadiga441@gmail.com>
2022-08-26 02:28:34 +0530

    added an updown animation to all planets with className PlanetSource

--- src/components/PlanetComponent.js
+++ src/components/PlanetComponent.js
@@ -1,44 +1,67 @@
-import { React, useEffect, useState } from 'react'
-import earth from "./assets/earth.svg";
+import { React, useState } from 'react'
 import "./PlanetComponent.css";
 import Sun from "./assets/Sun.svg";
 import Mercury from './Mercury';
 import Earth from './Earth';
-import { motion } from "framer-motion";
+import Header from "./Header";
+import Mars from './Mars';
+import Venus from './Venus';
 
 const PlanetComponent = () => {
-    const [planetNo, setPlanetNo] = useState(0);
+    const [planetNo, setPlanetNo] = useState(1);
+
+
     const Increment = () => {
-        setPlanetNo(planetNo + 1);
+        if (planetNo < 4)
+            setPlanetNo(planetNo + 1);
     }
     const Decrement = () => {
-        setPlanetNo(planetNo - 1);
+        if (planetNo > 1)
+            setPlanetNo(planetNo - 1);
     }
     const HandlePlanet = () => {
         switch (planetNo) {
             case 1:
                 return (
-                    <Mercury />
+                    <Mercury className="planetSource" />
                 )
-
             case 2:
-                return <Earth />
+                return <Venus className="planetSource" />
+            case 3:
+                return <Earth className="planetSource" />
+            case 4:
+                return <Mars className="planetSource" />
             default:
-                return <Earth />
+                return <Earth className="planetSource" />
+        }
+    }
+    const HandleName = () => {
+        switch (planetNo) {
+            case 1:
+                return "Mercury";
+            case 2:
+                return "Venus";
+            case 3:
+                return "Earth";
+            case 4:
+                return "Mars";
         }
     }
     return (
-        <div className='PlanetHolder'>
-            <div className='planet'>
-                <i className="fa-solid fa-angles-left" onClick={Increment}></i>
-                {HandlePlanet()}
-                <i className="fa-solid fa-angles-right" onClick={Decrement}></i>
-                <h4>Earth</h4>
+        <>
+            <Header />
+            <div className='PlanetHolder'>
+                <div className='planet'>
+                    <i className="fa-solid fa-angles-left planetHandlerIcons" onClick={Increment}></i>
+                    {HandlePlanet()}
+                    <i className="fa-solid fa-angles-right planetHandlerIcons" onClick={Decrement}></i>
+                    <h3 style={{ color: "#01fff0" }}>{HandleName()}</h3>
+                </div>
+                <div className='Sun'>
+                    <img src={Sun} className="SunSource" alt="sun.svg"></img>
+                </div>
             </div>
-            <div className='Sun'>
-                <img src={Sun} className="SunSource" alt="sun.svg"></img>
-            </div>
-        </div>
+        </>
     )
 }
 
